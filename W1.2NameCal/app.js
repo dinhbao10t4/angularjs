f43acfeafc16111a459f82ce50d58591ede1e692
Bao Truong <dinhbao10t4@gmail.com>
2018-03-19 10:38:21 +0700

    format code

--- W1.2NameCal/app.js
+++ W1.2NameCal/app.js
@@ -2,18 +2,17 @@
   'use strict';
 
   angular.module('helloWorld', [])
-  .controller('HelloWorldController', HelloController)
-  ;
+    .controller('HelloWorldController', HelloController);
 
-  function HelloController($scope){
+  function HelloController($scope) {
     $scope.name = "Coursera";
     $scope.value = calCharCode($scope.name);
 
-    $scope.displayNum = function(){
+    $scope.displayNum = function() {
       $scope.value = calCharCode($scope.name);
     }
 
-    function calCharCode(name){
+    function calCharCode(name) {
       var totalNameValue = 0;
       for (var i = 0; i < name.length; i++) {
         totalNameValue += name.charCodeAt(i);
